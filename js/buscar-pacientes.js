d70bee1c0b4a077ffac935723753e0b7dea20ed0
Rodrigo Viturino <viturino.souza1@gmail.com>
2018-06-09 16:51:47 -0300

    Capturando erros de requisicao
    Estamos capturando os erros de requisição, informando ao usuario o erro,
    onde foi criado um SPAN e adicionando uma class de 'invisivel' via
    javascript, é ativado com 'CONDICIONAL' SE clicar em 'BUSCAR PACIENTE'
    for TRUE, API é CARREGADA', SENAO mostra mensagem de erro.

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -8,14 +8,31 @@ let btnAdicionar = document.querySelector('#buscar-pacientes');
       xhr.open('GET','http://api-pacientes.herokuapp.com/pacientes');
       // Quando clicar no botão, ele vai CARREGAR a LISTA da API
       xhr.addEventListener('load',function(){
-          let resposta = xhr.response;
 
-          // REQUISIÇÂO COM O AJAX
-          let pacientes = JSON.parse(resposta);
-          // PARA CADA item do ARRAY, estamos pegando 1 por 1
-          pacientes.forEach(function(paciente){
-              adicionaPacienteNaTabela(paciente);
-          });
+          if(xhr.status == 200){
+            // Selecionando o 'SPAN' e adicionando a 'CLASS' invisivel
+            let erroAjax = document.querySelector('#erro-ajax');
+            erroAjax.classList.add('invisivel');
+
+            let resposta = xhr.response; // retorna OBJETO
+
+            // REQUISIÇÂO COM O AJAX
+            let pacientes = JSON.parse(resposta); // retorna ARRAY
+            
+            // PARA CADA item do ARRAY, estamos pegando 1 por 1
+            pacientes.forEach(function(paciente){
+                adicionaPacienteNaTabela(paciente);
+            });
+          } else {
+            console.log(xhr.status);
+            console.log(xhr.responseText);
+
+
+            erroAjax.classList.add('mensagemErro');
+            erroAjax.classList.remove('invisivel');
+          }
+
+
       });
       // vai ENVIAR a REQUISIÇÂO HTTP
       xhr.send();
