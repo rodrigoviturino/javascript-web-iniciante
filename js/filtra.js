ce052da35c3d46e98df05f6bfd466d380dbaba3d
Rodrigo Viturino <viturino.souza1@gmail.com>
2018-06-09 13:14:16 -0300

    Adicionando Expressao Regular
    Fazendo o filtro verificar igual o LIKE do sql, ele vai buscar por LETRA
    e nao mais pela PALAVRA COMPLETA.
    
    Co-Authored-By: Rodrigo Viturino <viturino.souza1@gmail.com>

--- js/filtra.js
+++ js/filtra.js
@@ -3,26 +3,32 @@ var campoFiltro = document.querySelector('#filtrar-tabela');
 campoFiltro.addEventListener("input",function(){
     var pacientes = document.querySelectorAll('.paciente');
 
-    if(this.value.length > 0){
-        
-        for (var i = 0; i < pacientes.length; i++) {
 
-            var paciente = pacientes[i];
-            var tdNome = paciente.querySelector('.info-nome');
-            var nome = paciente.textContent;
+    if(this.value.length > 0) {
 
-        if (nome != this.value) {
-            paciente.classList.add('invisivel');
-        } else {       
-            paciente.classList.remove('invisivel');
+      // Pegando todos itens do array
+      for(let i=0; i < pacientes.length; i++) {
+          // Pegando cada item pela Class e
+            let paciente = pacientes[i];
+            let tdNome = paciente.querySelector('.info-nome');
+            let nome = tdNome.textContent;
+            let expressao = new RegExp(this.value,"i");
+// Tive que inverter a logica porque ele estava colocando a class 'invisivel'
+            if(!expressao.test(nome)){
+            //if(nome != this.value){ // Neste caso o THIS é o PACIENTES[i], estamos dentro do BLOCO
+                paciente.classList.add('invisivel');
+            } else {
+                paciente.classList.remove('invisivel');
+            }
         }
-      }    
-    }else{
-        for(var i = 0; i < pacientes.length; i++){
-            var paciente = pacientes[i];
-            paciente.classList.remove('invisivel');
-        }
+    } else {
+      for(let i = 0; i < pacientes.length; i++){
+          let paciente = pacientes[i];
+          paciente.classList.remove('invisivel');
+      }
     }
+
+
 
 
 });
